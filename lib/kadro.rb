4eb0b2a7335ba756d543601beee38fea8cd2c411
Manuel Ortega <manuel@lasindias.coop>
2018-08-15 14:26:32 -0500

    Added a basic routing to controllers

--- lib/kadro.rb
+++ lib/kadro.rb
@@ -1,12 +1,32 @@
 require "kadro/version"
+require "kadro/routing"
+
 require "kadro/array"
 
 module Kadro
   class Application
     def call(env)
-      `echo debug > debug.txt`;
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+      `echo #{DateTime.now}: Executing action #{act} on controller #{klass.to_s} > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Kadro!"]]
+        [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
